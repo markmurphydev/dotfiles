11c03398551f7c1ee54bf59f438752c2145cf651
Mark Murphy <murphyml207@potsdam.edu>
2025-09-28 13:21:51 -0400

    checkin

--- emacs/init.el
+++ emacs/init.el
@@ -9,6 +9,7 @@
   ;; Enable use-package :ensure support for Elpaca.
   (elpaca-use-package-mode))
 (setopt use-package-always-ensure t)
+(setopt use-package-hook-name-suffix nil)
 
 ;; Make Emacs store autosave files in /tmp directory
 ;; https://www.reddit.com/r/emacs/comments/ym3t77/how_to_delete_auto_save_files_when_quitting_emacs/
@@ -41,44 +42,57 @@
   ;; Avoid errors about prefix keys
   (general-auto-unbind-keys)
   ;; Unbind keys from troubled modes
-  (setf (cdr dired-mode-map) nil)
   (setf (cdr help-mode-map) nil)
+  ;; Required for evil-collection
+  (setopt evil-want-collec nil)
   :config
   (evil-mode)
   :custom
   (evil-undo-system 'undo-redo)
+  (evil-move-beyond-eol t)
   :general
-  (general-def
-    ;; Command-backspace
-    "s-<backspace>" #'mark--kill-to-line-start
-    ;; minibuffer quit
-    "<escape>" #'keyboard-escape-quit)
-  (general-def minibuffer-mode-map
-    "M-<backspace>" #'backward-kill-word
-    "s-<backspace>" #'mark--kill-to-line-start)
-  (general-def dired-mode-map
-    "RET" #'dired-find-file)
+  ;; Unbinds
+  (general-unbind 'visual "s")
+  (general-define-key
+   ;; Command-backspace
+   "s-<backspace>" #'mark-kill-to-line-start
+   ;; minibuffer quit
+   "<escape>" #'keyboard-escape-quit)
+  (general-define-key
+   :keymaps 'minibuffer-mode-map
+   "M-<backspace>" #'backward-kill-word
+   "s-<backspace>" #'mark-kill-to-line-start)
   ;; Global keybindings
-  (general-def '(motion insert)
+  (general-define-key
+    :states 'motion
     "s-p" #'evil-paste-pop
-    "s-/" #'mark--comment-line)
-  (general-def 'motion
+    "s-/" #'mark-comment-line)
+  (general-define-key
+    :states 'motion
     "U" #'evil-redo
     "C-u" #'evil-scroll-up)
   (general-def 'motion
     :prefix "SPC"
+    "w s" #'evil-window-split
     "w d" #'evil-quit
     "w h" #'evil-window-left
     "w j" #'evil-window-down
     "w k" #'evil-window-up
     "w l" #'evil-window-right
+    "w L" #'evil-window-bottom-right
     "w v" #'evil-window-vsplit
     "r i" #'mark-reload-init
     "o" #'find-file
+    "p d" #'project-find-dir
+    "p b" #'project-switch-to-buffer
+    "p f" #'project-find-file
+    "p p" #'project-switch-project
     "f f" #'find-file
     "h f" #'describe-function
     "h k" #'describe-key
     "h v" #'describe-variable
+    "h b" #'describe-bindings
+    "h m" #'describe-mode
     ";" #'eval-expression
     "x" #'execute-extended-command
     "b b" #'consult-buffer
@@ -86,9 +100,50 @@
     "b d" #'kill-current-buffer
     "b n" #'next-buffer))
 
+;; == evil-surround ==
+;; https://github.com/emacs-evil/evil-surround
+(use-package evil-surround
+  :config
+  (global-evil-surround-mode 1)
+  :general
+  (general-define-key
+   :states 'visual
+   :keymaps 'evil-surround-mode-map
+   "s" #'evil-surround-region))
+
 ;; == evil-collection ==
-;; Vim keybindings outside of text buffers
-;; (use-package evil-collection :ensure (:ref "1cf0f9654bbb53a1093b545a64df299f4aca3f9d"))
+;; https://github.com/emacs-evil/evil-collection
+(use-package evil-collection
+  :config
+  (setopt evil-collection-key-blacklist '("SPC")))
+
+;; == dired overrides ==
+;; If you need to override evil keys for a major mode, use this method:
+(define-minor-mode dired-keys-mode
+  "Add override keybindings for dired mode.")
+
+(use-package dired
+  :ensure nil
+  :config
+  (setf (cdr dired-mode-map) nil)
+  :general
+  (general-define-key
+   :definer 'minor-mode
+   :states 'normal
+   :keymaps 'dired-keys-mode
+   "<return>" #'dired-find-file
+   "m" #'dired-mark
+   "d" #'dired-flag-file-deletion
+   "u" #'dired-unmark)
+  (general-define-key
+   :states 'normal
+   :keymaps 'dired-mode-map
+   :prefix "SPC m"
+   "d" #'dired-do-flagged-delete
+   "c d" #'dired-create-directory
+   "r" #'dired-do-rename)
+  :hook
+  (dired-mode-hook . dired-keys-mode))
 
 ;; == treesit-auto ==
 ;; https://github.com/renzmann/treesit-auto
@@ -162,7 +217,11 @@
   (corfu-auto t)
   (corfu-quit-no-match 'separator)
   :init
-  (global-corfu-mode))
+  (global-corfu-mode)
+  :general
+  (general-define-key
+   :keymaps 'corfu-map
+   "RET" nil))
 
 
 ;; == eldoc-box ==
@@ -171,6 +230,10 @@
   :bind
   (("s-l" . eldoc-box-help-at-point)))
 
+;; == which-key ==
+(which-key-mode 1)
+(setopt which-key-idle-delay 0.05)
+
 
 ;; ==== Major Modes ====
 
@@ -178,6 +241,57 @@
 ;; https://jblevins.org/projects/markdown-mode/
 (use-package markdown-mode
   :mode ("*\\.md\\'" . gfm-mode))
+
+;; == sly-mode ==
+;; Common lisp
+;; https://github.com/joaotavora/sly
+(use-package sly
+  :custom
+  (inferior-lisp-program "/opt/homebrew/bin/sbcl")
+  :general
+  (general-define-key
+   :states 'normal
+   :keymaps 'sly-mode-map
+   "g d" #'sly-edit-definition)
+  (general-define-key
+   :states 'normal
+   :keymaps 'sly-mode-map
+   :prefix "SPC"
+   "e e" #'mark-sly-eval-last-expression
+   "e r" #'sly-eval-region
+   "e f" #'sly-eval-defun
+   "e b" #'sly-eval-buffer
+   "e m" #'mark-sly-macroexpand-all
+   "m h s" #'sly-describe-symbol
+   "m h f" #'sly-describe-function)
+  (general-define-key
+   :states 'normal
+   :keymaps 'sly-mrepl-mode-map
+   "<return>" #'sly-mrepl-return
+   "SPC k" #'consult-history
+   "SPC c" #'sly-mrepl-clear-repl))
+
+;; == paredit ==
+(use-package paredit
+  :hook
+  ((lisp-mode-hook emacs-lisp-mode-hook). paredit-mode))
+
+;; == evil-cleverparens ==
+;; https://github.com/emacs-evil/evil-cleverparens?tab=readme-ov-file
+(use-package evil-cleverparens
+  :init
+  (setopt evil-cleverparens-use-s-and-S nil)
+  :hook
+  (paredit-mode-hook . #'evil-cleverparens-mode))
+
+
+;; == transient ==
+;; Needed to install separately for magit
+(use-package transient :ensure (:tag "v0.10.0"))
+
+
+;; == magit ==
+(use-package magit :ensure (:tag "v4.4.0"))
 
 
 ;; ==== Emacs settings ====
@@ -194,15 +308,24 @@
   :config
   (load-theme 'monokai t))
 
+;; == Follow symlinks ==
+;; https://stackoverflow.com/questions/15390178/emacs-and-symbolic-links
+(setopt vc-follow-symlinks t)
+
+;; == Scratch buffer ==
+;; Show scratch buffer on startup
+(setopt inhibit-startup-screen t)
+(setopt initial-scratch-message "")
+
 
 ;; ==== Commands ====
 
-(defun mark--kill-to-line-start ()
+(defun mark-kill-to-line-start ()
   "Kill to start of line."
   (interactive)
   (kill-line 0))
 
-(defun mark--comment-line ()
+(defun mark-comment-line ()
   "Comment line or active region, without moving the point."
   (interactive)
   (let* ((start-point (point)))
@@ -214,9 +337,24 @@
 	       (evil-visual-restore))
       (progn (comment-line 1)
 	     (goto-char start-point)))))
-  
 
 (defun mark-reload-init ()
   "Reload init.el"
   (interactive)
   (load-file user-init-file))
+
+(defun mark--eval-last (body)
+  (interactive)
+  (save-excursion
+    (evil-save-state
+      (evil-insert 1)
+      (right-char)
+      (call-interactively body))))
+
+(defun mark-sly-eval-last-expression ()
+  (interactive)
+  (mark--eval-last #'sly-eval-last-expression))
+
+(defun mark-sly-macroexpand-all ()
+  (interactive)
+  (mark--eval-last #'sly-macroexpand-all))
